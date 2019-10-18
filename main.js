ab867c50944c3051334bf38eb8eaea20a0f9d00c
dtyurin01 <30621310+dtyurin01@users.noreply.github.com>
2019-10-18 21:42:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,108 @@
-window.addEventListener('load', ()=> {
-	let long;
-	let lat;
-	let temperatureDescription = document.querySelector('.temperature-description');
-	let temperatureDegree = document.querySelector('.temperature-degree');
-	let locationTimezone = document.querySelector('.location-timezone');
-	let temperatureSection =document.querySelector('.temperature'); 
-	const temperatureSpan = document.querySelector('.temperature span');
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
 
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition
-		(position =>{
-			long = position.coords.longitude;
-			lat = position.coords.latitude;
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro-button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
 
-			const proxy = 'https://cors-anywhere.herokuapp.com/';
-			const api = `${proxy}https://api.darksky.net/forecast/29b323bb34dc71319db146ef391fd39b/${lat},${long}`; 
-			
-			fetch(api)
-			.then(response => {
-				return response.json();
-			})
-			.then(data =>{
-				const {temperature, summary, icon } = data.currently;
-				// Set elements from API
-				temperatureDegree.textContent = temperature;
-				temperatureDescription.textContent = summary; 
-				locationTimezone.textContent = data.timezone;
-				// Formula for Celsius
-				let celsius = (temperature - 32) * (5 / 9);
-				// Set icon
-				setIcons(icon, document.querySelector('.icon'));
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add('fadeIn');
+        });
+    };
+    // Match
+    const playMatch = () => {
+        const options = document.querySelectorAll(".option button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll('.hands img');
 
-				// Change temperature to Celsius/Farenheit
-				 temperatureSection.addEventListener("click", () =>{
-				 	if(temperatureSpan.textContent === "F"){
-				 		temperatureSpan.textContent = "C";
-				 		temperatureDegree.textContent = Math.floor(celsius);
-				 	}else{
-				 		temperatureSpan.textContent = "F";
-				 		//temperatureDegree.textContent = temperature;!!
-				 	}
-				 });
-			});
-		});
-	}else{
-		h1.textContent = 'this is not working cause ur browser doenst support this site';
-	}
-	function setIcons(icon, iconID) {
-		const skycons = new Skycons({color:"white"});
-		const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-		skycons.play();
-		return skycons.set(iconID, Skycons[currentIcon]);
-	}
-});
+        hands.forEach((hand) => {
+            hand.addEventListener('animationend', function () {
+                this.style.animation = "";
+            })
+        })
+        // Computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach((option) => {
+            option.addEventListener('click', function () {
+                const computerNumber = Math.round(Math.random() * 2);
+                const computerChoice = computerOptions[computerNumber];
+
+                compareHands(this.textContent, computerChoice);
+                // Images
+                playerHand.src = `./img/${this.textContent}.png`;
+                computerHand.src = `./img/${computerChoice}.png`;
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+            });
+        });
+
+    };
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
+    const compareHands = (playerChoice, computerChoice) => {
+        const winner = document.querySelector('.winner');
+        // tie
+        if (playerChoice === computerChoice) {
+            winner.textContent = 'IT is a tie';
+            return;
+        }
+        // rock
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'player wins'
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'computer wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        // paper
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Computer wins'
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'Player wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+        // scissors
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winner.textContent = 'computer wins'
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'player wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+    }
+    // Call all the inner function
+    startGame();
+    playMatch()
+};
+// there we start
+game()
